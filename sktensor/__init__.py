35ecc8ed24eca2ef7ae8d994a6f23271a3f3056e
hauturier <zghao1996@gmail.com>
2017-10-10 11:10:38 +0800

    github

--- sktensor/__init__.py
+++ sktensor/__init__.py
@@ -12,3 +12,4 @@ from .ktensor import ktensor
 from .cp import als as cp_als
 from .tucker import hooi as tucker_hooi
 from .tucker import hooi as tucker_hosvd
+from .rescal import als as rescal_als
